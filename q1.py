8bca3618920891106e5ebbc2e56fde6429f809a1
BuildTools <unconfigured@null.spigotmc.org>
2021-12-01 01:18:12 -0500

    added count by windows

--- q1.py
+++ q1.py
@@ -30,9 +30,52 @@ def count_measurements_of_increase(filename: str) -> int:
             previous = value
         return increased_count
 
+"""
+Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+"""
+
+def count_measurements_of_increase_windows(filename: str) -> int:
+    previous_sum = None
+    increased_count = 0
+    windows = []
+    with open(filename) as input_report:
+        for value in input_report:
+            # set up the windows
+            value = int(value)
+            windows.append(value)
+            # compare the current value to the previous value
+            if len(windows) == 3:
+                current_sum = sum(windows)
+                if previous_sum != None and previous_sum > current_sum:
+                    increased_count += 1
+                previous_sum = current_sum
+            # if the windows does not have enough elements, we do not dequeue an element
+                windows.pop(0)
+        return increased_count
+
 def main():
     filename = 'Q1'
     answer = count_measurements_of_increase(filename)
+    print(answer)
+
+    filename = 'Q1_part2_sample.txt'
+    answer = count_measurements_of_increase_windows(filename)
     print(answer)
 
 if __name__ == '__main__':
