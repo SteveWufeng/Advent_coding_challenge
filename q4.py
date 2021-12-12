2dec3a0b82fc44b7727afa7a629327a81a37310b
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2021-12-12 15:51:29 -0500

    added check_bingo

--- q4.py
+++ q4.py
@@ -6,14 +6,42 @@ class Board:
         self.draws = draws
         self.marked = []
         self.index = 0
+        self.bingo = False
 
     def check_bingo(self) -> bool:
         """check the current board and see if there is a bingo"""
+        if self.bingo == True:  # if the table has already been checked return True to save resource
+            return True
+
+        # check horizontally
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j] not in self.marked:
+                    break
+                if j == len(self.board[i])-1:
+                    self.bingo = True
+                    return True
+            
+        # check verticle
+        for i in range(len(self.board[0])):
+            for j in range(len(self.board)):
+                if self.board[i][j] not in self.marked:
+                    break
+                if j == len(self.board)-1:
+                    self.bingo = True
+                    return True
         
+        return False    # return false if neither loop found a bingo.
 
     def mark_board(self) -> None:
         """Mark the called value"""
-        pass
+        draw_val = self.draws[self.index]
+        self.index += 1
+        for row in self.board:
+            if draw_val in row:
+                self.marked.append(draw_val)
+                return
+        
 
 def initialize_bingo(filename, tracker: list):
     table = []
