93078b324092ea5752c7ad31010c03f14e1e2dc1
BuildTools <unconfigured@null.spigotmc.org>
2022-01-06 01:46:44 -0500

    completed test with sample

--- q7.py
+++ q7.py
@@ -22,10 +22,38 @@ def initialize(filename): # returns max, min, set of crab's cordinate
 
     return max, min, crab_cords
 
+def binary_search(max, min, cords):
+    if max == min:  # base case
+        return max
+
+    mid = min + (max-min)//2
+    mid_small = min + (mid//2)
+    mid_large = mid + (mid//2)
+
+    # find the total fuel for mid
+    total_fuel_mid = 0
+    total_fuel_small = 0
+    total_fuel_large = 0 
+    for cord in cords:
+        cord = int(cord)
+        total_fuel_mid += abs(cord-mid) # find the difference from the cord to our mid
+        total_fuel_small += abs(cord-mid_small) # also check the left
+        total_fuel_large += abs(cord-mid_large) # and the right
+
+    # determine which way to move(binary search)
+    if total_fuel_small <= total_fuel_mid:    # if the smaller side uses less fuel
+        return binary_search(mid_small, min, cords) # run binary search again using the small side
+    elif total_fuel_large <= total_fuel_mid:    # if the bigger side uses less fuel
+        return binary_search(max, mid_large, cords) # run binary search on the big side
+    else:
+        return mid
+
 def main():
     filename = 'sample.txt'
     max, min, cords = initialize(filename)
-    print(max, min, cords)
+    # print(max, min, cords)
+    answer = binary_search(max, min, cords)
+    print(answer)
 
 if __name__ == '__main__':
     main()
