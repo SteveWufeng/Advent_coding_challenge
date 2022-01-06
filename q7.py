2113ea846bdfd3ab16dbd79419c861ff85e3daa7
BuildTools <unconfigured@null.spigotmc.org>
2022-01-05 21:25:31 -0500

    added some notes on Q7

---
+++ q7.py
@@ -0,0 +1,5 @@
+"""
+Idea:
+find max and min
+use binary search to look for the answer
+"""
--- test.py
+++ test.py
@@ -1,14 +1,19 @@
-import pyautogui
+# import pyautogui
 import time
-import random
+# import random
 
+# time.sleep(2)
+
+# for i in range(36):
+#     print('run')
+#     # pyautogui.press('num2')
+#     pyautogui.press('num2')
+#     time.sleep(random.randint(20,32))
+#     pyautogui.press('num2')
+#     wait = random.randint(30,60)
+#     time.sleep(wait)
+import ctypes
+
+# Move cursor to x 500 and y 500
 time.sleep(2)
-
-for i in range(36):
-    print('run')
-    # pyautogui.press('num2')
-    pyautogui.press('num2')
-    time.sleep(random.randint(20,32))
-    pyautogui.press('num2')
-    wait = random.randint(30,60)
-    time.sleep(wait)
+ctypes.windll.user32.SetCursorPos(500,500)
