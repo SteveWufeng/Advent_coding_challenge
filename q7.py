fb068789eb4bc15be8d6193ba317a882ef7a1627
BuildTools <unconfigured@null.spigotmc.org>
2022-01-06 00:50:50 -0500

    finished first step

--- q7.py
+++ q7.py
@@ -3,3 +3,29 @@ Idea:
 find max and min
 use binary search to look for the answer
 """
+
+def initialize(filename): # returns max, min, set of crab's cordinate
+    # process cordinate
+    with open(filename) as file:
+        crab_cords = next(file)             # read the first line
+        crab_cords = crab_cords.split(',')  # split with comma
+    
+    # find max and min
+    max = 0
+    min = 0
+    for cord in crab_cords:
+        cord = int(cord)
+        if cord > max:
+            max = cord
+        if cord < min:
+            min = cord
+
+    return max, min, crab_cords
+
+def main():
+    filename = 'sample.txt'
+    max, min, cords = initialize(filename)
+    print(max, min, cords)
+
+if __name__ == '__main__':
+    main()
